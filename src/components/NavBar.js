9b5ba9a55247b16bb52540d8dc5e5f52fdac31a5
karthik <dk.karthikd9@gmail.com>
2024-01-06 22:55:24 +0530

    client:added Navbar with react bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import {Navbar, Container, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 const NavBar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
-    </div>
+    <Navbar 
+        bg="primary" 
+        variant="dark"
+        sticky="top"
+    >
+        <Container>
+        <Navbar.Brand as={Link} to="/">News App</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+                Signed in as: Mark Otto
+            </Navbar.Text>
+            <Button variant="dark">Logout</Button>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+</div>
   )
 }
 
