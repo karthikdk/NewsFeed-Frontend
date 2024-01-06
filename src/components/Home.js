4405c116b290a356efc6b350aeec03ca8d414904
karthik <dk.karthikd9@gmail.com>
2024-01-06 22:48:50 +0530

    client:added Navbar and Login and Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Home = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1>Home</h1>
     </div>
   )
 }
 
-export default App
+export default Home
