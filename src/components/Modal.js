8cd6a006bb8058e75c636882f10c6e4a125232cd
karthik <dk.karthikd9@gmail.com>
2024-01-07 21:46:20 +0530

    login and registration modal setup

---
+++ src/components/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import {Modal} from 'react-bootstrap'
+
+const ModalComponet = () => {
+  return (
+    <div>
+      <h1>Modal</h1>
+    </div>
+  )
+}
+
+export default ModalComponet
