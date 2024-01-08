96d20c25cf3c0b4e5fd7a8940f620d19affdec7f
karthik <dk.karthikd9@gmail.com>
2024-01-08 20:21:21 +0530

    frontend:created source action reducer and store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,9 @@ import loginToggleReducer from '../reducers/loginToggleReducer'
 import registerToggleReducer from '../reducers/registerToggleReducer'
 import pageNoReducer from '../reducers/pageNumberReducer'
 import postsPerPageReducer from '../reducers/postsPerPage'
+import articlesReducer from '../reducers/newsArticleReducer'
+import categoryReducer from '../reducers/categoryRedcuer'
+import sourceReducer from '../reducers/sourceReducer'
 
 const configureStore=()=>{
     return createStore(combineReducers({
@@ -14,7 +17,10 @@ const configureStore=()=>{
         loginToggle:loginToggleReducer,
         registerToggle:registerToggleReducer,
         pageNo:pageNoReducer,
-        postsPerPage:postsPerPageReducer
+        postsPerPage:postsPerPageReducer,
+        articles:articlesReducer,
+        category:categoryReducer,
+        source:sourceReducer
     }),applyMiddleware(thunk))
 }
 export default configureStore
