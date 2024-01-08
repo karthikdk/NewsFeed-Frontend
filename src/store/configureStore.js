cff781f8b86a16e23d22b286f307d80bd515e49c
karthik <dk.karthikd9@gmail.com>
2024-01-08 19:13:39 +0530

    frontend:posts per page redux and store setup in redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,13 +4,17 @@ import isLoggedReducer from '../reducers/isLoggedReducer'
 import userReducer from '../reducers/userReducer'
 import loginToggleReducer from '../reducers/loginToggleReducer'
 import registerToggleReducer from '../reducers/registerToggleReducer'
+import pageNoReducer from '../reducers/pageNumberReducer'
+import postsPerPageReducer from '../reducers/postsPerPage'
 
 const configureStore=()=>{
     return createStore(combineReducers({
         user:userReducer,
         isLogged:isLoggedReducer,
         loginToggle:loginToggleReducer,
-        registerToggle:registerToggleReducer
+        registerToggle:registerToggleReducer,
+        pageNo:pageNoReducer,
+        postsPerPage:postsPerPageReducer
     }),applyMiddleware(thunk))
 }
 export default configureStore
