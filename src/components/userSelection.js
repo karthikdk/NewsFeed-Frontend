ab6fa60d345cc5c6de0804db171ab2877bcc4c7a
karthik <dk.karthikd9@gmail.com>
2024-01-08 18:46:25 +0530

    frontend:redux source and category action setup with userselection

--- src/components/userSelection.js
+++ src/components/userSelection.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+
+import { setSource } from '../actions/sourceActions'
+import { setCategory } from '../actions/categoryActions'
+
+
 
 const UserSelection = () => {
+    const categories = ['latest', 'india', 'technology', 'cricket']
+    const dispatch = useDispatch()
+
+    const category = useSelector((state)=>{
+        return state.category
+    })
+    const source = useSelector((state)=>{
+        return state.source
+    })
+   
+    const handleChange = (e) => {
+        const name = e.target.name
+        if(name === 'source'){
+            dispatch(setSource(e.target.value))
+        } else if(name === 'category'){
+            dispatch(setCategory(e.target.value))
+        }
+    }
   return (
     <div className="mt-3 mb-4">
     <h2 className="text-center">You are reading the {category.charAt(0).toUpperCase() + category.slice(1)} articles from {source}</h2>
         <div className="d-flex justify-content-between mt-3">
-        {/* <form>
-            <label>Articles per page</label>
-            <select 
-                className="form-control"
-                value={postsPerPage} 
-                onChange={(e)=>dispatch(setPostsPerPage(e.target.value))}
-            >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-            </select>
-        </form> */}
         <form className="d-flex justify-content-start">
             <div className="form-group  me-5">
                 <label>Change the Source of Articles</label>
@@ -28,7 +39,6 @@ const UserSelection = () => {
                     name="source"
                 >
                     <option value="TOI">Times of India</option>
-                    <option value="NDTV">NDTV</option>
                 </select>
             </div>
             <div className="form-group">
