6c9bd1f2bda087366e0a1478ddda2d4375643edb
karthik <dk.karthikd9@gmail.com>
2024-01-08 20:16:22 +0530

    frontend:created news list component with articles

---
+++ src/components/NewsList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import startGetArticles from '../actions/newsArticleActions'
+
+const NewsList = (props) => {
+    const dispatch = useDispatch()
+    const {source, category, pageNo, postsPerPage, articles} = useSelector((state)=>{
+        return state
+    })
+    useEffect(()=>{
+        dispatch(startGetArticles(source, category, pageNo, postsPerPage))
+    },[source, category, pageNo, postsPerPage, dispatch])
+  return (
+    <div>
+         {articles.length  ? (
+                    <div>
+                        {articles.map(article => {
+                            return <ArticleItem
+                                key={article._id}
+                                {...article}
+                            />
+                        })}
+                    </div>
+                ) : (
+                    <div style={{height : '45vh', textAlign : 'center', marginTop : '200px'}}>
+                        <h2>Nothing to show here.</h2>
+                        <h2>Go Back.</h2>
+                    </div>
+                )
+            }
+    </div>
+  )
+}
+
+export default NewsList
