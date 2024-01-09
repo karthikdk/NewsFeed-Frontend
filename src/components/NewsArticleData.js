36f579dbcb620b96fe80d57cd44809ab449da55a
karthik <dk.karthikd9@gmail.com>
2024-01-09 19:29:42 +0530

    frontend:newsArticle card setup created

--- src/components/NewsArticleData.js
+++ src/components/NewsArticleData.js
@@ -1,39 +1,19 @@
 import React from "react"
+import { Button, Card } from "react-bootstrap"
 
 const NewsArticle = (props) => {
     const {title,pubDate,link,description} = props
-
-    const backgroundStyle = {
-        height : '250px', 
-        backgroundSize : 'cover', 
-        backgroundPosition : "0% 25%" ,
-        width:'600px'
-    }
-
-    const anchorStyle = {
-        textDecoration : 'none',
-        color : 'white'
-    }
     return (
-        <div className="card bg-dark text-light mb-3 d-inline-block border-dark mt-3 shadow my-3 mx-3 px-2 py-2 col-sm-6" style={backgroundStyle}>
-            <div className="card-img-overlay" >
-                <h5 className="card-title">{title}</h5>
-                <div className="d-flex flex-column justify-content-between" style={{height : "90%"}}>
-                    <p className="card-text">{description}</p>
-                    <div className="d-flex justify-content-between">
-                        <p className="card-text">Published : {pubDate?.split('T').join(' at ')} +5:30 IST</p>
-                        <span className="btn btn-danger btn-sm">
-                            <a  target="_blank" 
-                                href={link} 
-                                style={anchorStyle}
-                                rel="noreferrer"
-                            >details
-                            </a>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Card style={{ width: '365px',height:'220px'}} className="mb-3 my-3 mx-3 d-inline-block px-2 py-2 col-md-3 bg-dark text-light">
+        <Card.Body>
+          <Card.Title>{title.slice(0,50)}</Card.Title>
+          <Card.Text>
+            {description?description.slice(0,80):"...."}
+          </Card.Text>
+          <p className="card-text">Published : {pubDate?.split('T').join(' at ')} +5:30 IST</p>
+          <Button variant="primary" href={link} target="_blank">More Details</Button>
+        </Card.Body>
+      </Card>
     )
 }
 
